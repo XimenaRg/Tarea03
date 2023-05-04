f71ce7ca8f6c859093d63973b3bf447271db6ab7
XimenaRg <124558516+XimenaRg@users.noreply.github.com>
2023-05-03 21:46:28 -0600

    Clase_Pregunta

---
+++ clase_pregunta.py
@@ -0,0 +1,23 @@
+class Pregunta:
+    """
+    Clase que representa una pregunta de un examen
+
+    Atributos:
+    __pregunta (str): La pregunta a realizar
+    __opciones (list): Una lista con las opciones de respuesta
+    __respuesta_correcta (int): El índice de la respuesta correcta en la lista de opciones
+    """
+        
+    def __init__(self, pregunta:str, opciones:list, respuesta_correcta:int):
+        """
+        Constructor de la clase Pregunta.
+
+        Args:
+        pregunta (str): La pregunta a realizar
+        opciones (list): Una lista con las opciones de respuesta
+        respuesta_correcta (int): El índice de la respuesta correcta en la lista de opciones
+        """
+
+        self.pregunta = pregunta
+        self.opciones = opciones
+        self.respuesta_correcta = respuesta_correcta
